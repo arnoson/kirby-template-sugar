01aa9e4af71d06eac52db4d69734e8b809a446b3
arnoson <hallo@arno-schlipf.info>
2023-03-31 02:29:35 +0200

    chore: improve output

--- src/transform.ts
+++ src/transform.ts
@@ -1,35 +1,89 @@
 import { Parser } from 'htmlparser2'
 import MagicString from 'magic-string'
-import { serializeToPhp } from './utils'
+
+const entriesToPhp = (entries: Record<string, any>) =>
+  entries.map(([key, value]) => `'${key}' => ${value}`).join(', ')
+
+const resolveValue = (value: string) => {
+  const match = value.match(/^<\?=(.* )\?>$/s)
+  const valueIsPhp = !!match
+  return valueIsPhp ? match[1].trim() : `'${value}'`
+}
+
+const parseLine = (line: string, attributes: Record<string, string>) => {
+  const matches = line.match(/[\s\S](@?[\w-]+)="/g)
+  if (!matches) return
+
+  const props = []
+  const attr = []
+  const indent = line.match(/^[\s\t]*/)?.[0] ?? ''
+
+  for (const match of matches) {
+    const key = match.slice(0, -2).trim()
+    const isProp = key.startsWith('@')
+    const value = resolveValue(attributes[key])
+    const name = isProp ? key.slice(1) : key
+    if (isProp) props.push([name, value])
+    else attr.push([name, value])
+  }
+
+  return { props, attr, indent }
+}
 
 const openSnippet = (
+  tagHtml: string,
   tagName: string,
   isSelfClosing: boolean,
   attributes: Record<string, string>
 ) => {
   const name = tagName.slice(8)
+  const lines = tagHtml.split('\n')
+  const data: string[] = []
+  const allAttr = []
 
-  const snippetData = {}
-  const htmlAttributes = {}
-  for (let [key, value] of Object.entries(attributes)) {
-    if (key.startsWith('@')) {
-      key = key.slice(1)
+  let lastIndent = ''
 
-      const match = value.match(/^<\?=(.* )\?>$/s)
-      const valueIsPhp = !!match
-      value = valueIsPhp ? match[1].trim() : `'${value}'`
+  for (const [i, line] of lines.entries()) {
+    const { props, attr, indent } = parseLine(line, attributes) ?? {}
+    const isFirstLine = i === 0
+    const isLastLine = i === lines.length - 1
 
-      snippetData[key] = value
-    } else {
-      htmlAttributes[key] = `'${value}'`
+    lastIndent = indent ?? lastIndent
+
+    if (attr) allAttr.push(...attr)
+
+    if (isLastLine && !props?.length) {
+      if (allAttr.length)
+        data.push(`${lastIndent}${`'attr' => [${entriesToPhp(allAttr)}]`}`)
+      else data.push('')
+      continue
     }
+
+    if (!props?.length && !attr?.length) {
+      data.push('')
+      continue
+    }
+
+    if (!props.length) {
+      if (isFirstLine) {
+        data.push('')
+      } else {
+        const keys = attr.map(([key]) => `[${key}]`)
+        data.push(`${indent}// ${keys.join(' ')}`)
+      }
+      continue
+    }
+
+    if (isLastLine && allAttr.length) {
+      props.push(['attr', `[${entriesToPhp(allAttr)}]`])
+    }
+
+    const comma = isLastLine ? '' : ','
+    data.push(`${indent}${entriesToPhp(props)}${comma}`)
   }
 
-  if (Object.keys(htmlAttributes).length)
-    snippetData['attr'] = serializeToPhp(htmlAttributes)
-
   const args = [`'${name}'`]
-  if (Object.keys(snippetData).length) args.push(serializeToPhp(snippetData))
+  if (data.length) args.push(`[${data.join('\n')}]`)
   if (!isSelfClosing) args.push('slots: true')
 
   return `<?php snippet(${args.join(', ')}); ?>`
@@ -55,25 +109,18 @@ export const transform = (input: string) => {
 
   const parser = new Parser({
     onopentag(tagName, attributes) {
-      console.log(attributes)
       const isSelfClosing = input[parser.endIndex - 1] === '/'
-
-      const text = input.slice(parser.startIndex, parser.endIndex + 1)
-      const lines = text.split('\n').length
+      const { startIndex, endIndex } = parser
+      const tagHtml = input.slice(parser.startIndex, parser.endIndex + 1)
 
       const overwrite = tagName.startsWith('snippet:')
-        ? openSnippet(tagName, isSelfClosing, attributes)
+        ? openSnippet(tagHtml, tagName, isSelfClosing, attributes)
         : tagName.startsWith('slot')
         ? openSlot(tagName)
         : null
 
-      const { startIndex, endIndex } = parser
       if (overwrite !== null)
-        output.overwrite(
-          startIndex,
-          endIndex + 1,
-          overwrite + '\n'.repeat(lines - 1)
-        )
+        output.overwrite(startIndex, endIndex + 1, overwrite)
     },
 
     onclosetag(tagName, isImplied) {
